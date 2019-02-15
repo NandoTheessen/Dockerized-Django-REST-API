879e4306b7607ed3514b34cc1d4b685dac6e04b9
NandoTheessen <nando.theessen@gmail.com>
2019-02-15 16:40:32 +0100

    Adds music app

---
+++ music/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Songs
+from .serializers import SongsSerializer
+
+# Create your tests here.
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_song(title="", artist=""):
+        if title != "" and artist != "":
+            Songs.objects.create(title=title, artist=artist)
+
+    def setup(self):
+        self.create_song("like glue", "sean paul")
+        self.create_song("candy man", "kool savas")
+        self.create_song("deine mutter", "kool savas")
+        self.create_song("immer wieder", "mo trip")
+
+
+class GetAllSongsTest(BaseViewTest):
+
+    def test_get_all_songs(self):
+
+        reponse = self.client.get(
+            reverse("songs-all", kwargs={"version": "v1"})
+        )
+
+        expected = Songs.objects.all()
+        serialized = SongsSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
